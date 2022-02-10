b1307ad7c62d067e577dbd062fefd0b7b1ec0c06
Szofi Retei-Nagy <74651930+Szofi87@users.noreply.github.com>
2022-02-11 00:20:49 +0100

    Add files via upload

---
+++ diagonalStar/DiagonalStar.java
@@ -0,0 +1,27 @@
+package diagonalStar;
+
+public class DiagonalStar {
+
+    public static void printSquareStar(int number){
+        if( number < 5) {
+            System.out.println("Invalid Value");
+            return;
+        }else {
+
+        for (int row = 1; row <= number; row++){
+            for (int col = 1; col <= number; col++){
+                if (row == 1 || row == number || col == 1 || col == number){
+                    System.out.print("*");
+                } else if (row == col) {
+                    System.out.print("*");
+                } else if (col == (number - row + 1)) {
+                    System.out.print("*");
+                } else
+                    System.out.print(" ");
+            }
+            System.out.println();
+
+        }
+    }
+    }
+}
