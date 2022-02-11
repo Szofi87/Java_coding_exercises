eb6ba94a1ccfa9e83d5a21218e1db9bf23c0b7aa
Szofi Retei-Nagy <74651930+Szofi87@users.noreply.github.com>
2022-02-11 03:12:33 +0100

    Add files via upload

---
+++ Sets/DwarfPlanet.java
@@ -0,0 +1,8 @@
+package Sets;
+
+public class DwarfPlanet extends HeavenlyBody{
+
+    public DwarfPlanet(String name, double orbitalPeriod) {
+        super(name, orbitalPeriod, BodyTypes.DWARF_PLANET);
+    }
+}
